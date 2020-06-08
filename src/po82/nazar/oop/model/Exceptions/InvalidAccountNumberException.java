823f4d3bbea28eb5438d6fb41db77e72bc775fc3
nazar <nazarenko125@gmail.com>
2020-06-08 11:39:19 +0400

    Initial commit

---
+++ src/po82/nazar/oop/model/Exceptions/InvalidAccountNumberException.java
@@ -0,0 +1,8 @@
+package po82.nazar.oop.model.Exceptions;
+
+public class InvalidAccountNumberException extends Exception{
+
+    public InvalidAccountNumberException(String message){
+        super(message);
+    }
+}
